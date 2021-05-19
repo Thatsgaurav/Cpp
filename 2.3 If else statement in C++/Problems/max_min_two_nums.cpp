bae0ec2f9e1c551610d0018efde4af3925da38a9
Gaurav Singh <0201csai117@niet.co.in>
2021-05-19 20:21:24 +0530

    commit

--- 2.3 If else statement in C++/Problems/max_min_two_nums.cpp
+++ 2.3 If else statement in C++/Problems/max_min_two_nums.cpp
@@ -1,1 +1,28 @@
 // Program to find maximum, minimum among two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n1, n2;
+    cin >> n1 >> n2;
+
+    int max, min;
+    if (n1 > n2)
+    {
+        max = n1;
+        min = n2;
+    }
+    else
+    {
+        max = n2;
+        min = n1;
+    }
+
+    cout << "Max" << max << endl;
+    cout << "Min" << min << endl;
+
+    return 0;
+}
