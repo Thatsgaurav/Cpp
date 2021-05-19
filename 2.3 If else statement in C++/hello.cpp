8a0e493829ee3412fcf797d1333869e4eafa86f5
Gaurav Singh <0201csai117@niet.co.in>
2021-05-19 17:42:00 +0530

    commit

--- 2.3 If else statement in C++/hello.cpp
+++ 2.3 If else statement in C++/hello.cpp
@@ -11,14 +11,16 @@ int main()
     int savings;
     cin >> savings;
 
-    if (savings > 5000)
-    {
-        cout << "Neha\n";
-    }
-    else
-    {
+    if (savings > 5000) {
+        if(savings > 10000) {
+            cout << "Roadtrip with neha\n";
+        } else {
+            cout << "Shoping with neha\n";
+        }
+    } else if (savings > 2000) {
         cout << "Rashmi\n";
-    }
-
+    } else {
+        cout << "Friends\n";
+    }   
     return 0;
 }
