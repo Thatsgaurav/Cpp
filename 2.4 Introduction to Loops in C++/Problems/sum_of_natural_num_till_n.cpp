60f4423b1c11ab6823efe939960b046577a194b8
Gaurav Singh <0201csai117@niet.co.in>
2021-05-21 04:08:03 +0530

    commit

--- 2.4 Introduction to Loops in C++/Problems/sum_of_natural_num_till_n.cpp
+++ 2.4 Introduction to Loops in C++/Problems/sum_of_natural_num_till_n.cpp
@@ -1,1 +1,21 @@
 // Program to find the sum of natural numbers till n.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    for (int counter = 1; counter <= n; counter++)
+    {
+        sum = sum + counter;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
