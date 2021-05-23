47b13bf7ad8fb6d38a04ca2ded0bfdfff35c5fc9
Gaurav Singh <0201csai117@niet.co.in>
2021-05-23 23:16:33 +0530

    commit

--- 4.2 Advanced Pattern Questions in C++/rhombus.cpp
+++ 4.2 Advanced Pattern Questions in C++/rhombus.cpp
@@ -8,5 +8,17 @@ int main()
     int n;
     cin >> n;
 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     return 0;
 }
