899a92b10fbba69d94f5c96eb9ca2a4b9a0e037b
Gaurav Singh <0201csai117@niet.co.in>
2021-05-24 10:27:15 +0530

    commit

--- 5.2 How to reverse a number/Problems/prime.cpp
+++ 5.2 How to reverse a number/Problems/prime.cpp
@@ -1,1 +1,25 @@
 // Write a program to check if a number is prime or not.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    bool flag = 0;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "Non-prime" << endl;
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+    {
+        cout << "prime" << endl;
+    }
+    return 0;
+}
