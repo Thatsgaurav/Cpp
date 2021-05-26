54cc898dd2c2a85de7126d4047daf53fadf8ae73
Gaurav Singh <0201csai117@niet.co.in>
2021-05-26 05:55:11 +0530

    commit

--- 6.1 Functions in C++/printnumber.cpp
+++ 6.1 Functions in C++/printnumber.cpp
@@ -1,1 +1,20 @@
 // Write a program to print a given number using functions.
+
+#include <iostream>
+using namespace std;
+
+void print(int a)
+{
+    cout << a << endl;
+    return;
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    print(a);
+
+    return 0;
+}
