81c044214f26149eac0fde6c345578161ecd39bd
Gaurav Singh <0201csai117@niet.co.in>
2021-05-26 05:45:39 +0530

    commit

---
+++ 6.1 Functions in C++/add-2-numbers.cpp
@@ -0,0 +1,20 @@
+// Write a program to add 2 numbers using functions.
+
+#include <iostream>
+using namespace std;
+
+int add(int num1, int num2)
+{
+    int sum = num1 + num2;
+    return sum;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << add(a, b) << endl;
+
+    return 0;
+}
