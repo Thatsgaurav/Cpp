b0f5af191794835d61a9f785c856a8b84ceec720
Gaurav Singh <0201csai117@niet.co.in>
2021-06-04 23:40:41 +0530

    commit

--- 7.1 Time Complexity C++/Problems/Linear Time Complexity.cpp
+++ 7.1 Time Complexity C++/Problems/Linear Time Complexity.cpp
@@ -1,1 +1,17 @@
 // Linear Time Complexity. O(n)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        a = a + 1;
+    }
+
+    return 0;
+}
