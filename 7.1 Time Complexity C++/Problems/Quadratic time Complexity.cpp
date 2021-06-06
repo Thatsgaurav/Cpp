9902b3e9b726c0322095466ff8e20b792c3b9373
Gaurav Singh <0201csai117@niet.co.in>
2021-06-06 19:10:18 +0530

    commit

--- 7.1 Time Complexity C++/Problems/Quadratic time Complexity.cpp
+++ 7.1 Time Complexity C++/Problems/Quadratic time Complexity.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    int a = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            a = a + 1;
+        }
+    }
 
     return 0;
 }
