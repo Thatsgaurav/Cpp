b5e7477de51c6130b0ae26aeb6e4fa960088b502
Gaurav Singh <0201csai117@niet.co.in>
2021-06-06 22:11:16 +0530

    commit

--- 7.1 Time Complexity C++/Problems/Time complexity-Olog-n.cpp
+++ 7.1 Time Complexity C++/Problems/Time complexity-Olog-n.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main()
 {
+    int n = 0;
+    cin >> n;
+    int a = 0, i = n;
+    while (i >= 1)
+    {
+        a = a + 1;
+        i /= 2;
+    }
 
     return 0;
 }
