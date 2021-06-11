aff22fbb5191639c6be064486435f6d549bd10ff
Gaurav Singh <0201csai117@niet.co.in>
2021-06-11 21:21:12 +0530

    commit

--- 8.1 Introduction to Arrays in C++/Problems/Running sum of an Array.cpp
+++ 8.1 Introduction to Arrays in C++/Problems/Running sum of an Array.cpp
@@ -1,1 +1,17 @@
 // Running sum of an Array
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {2, 5, 7, 8, 2, 6, 9};
+    int n = 7, sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    cout << "The array sum is " << sum;
+
+    return 0;
+}
