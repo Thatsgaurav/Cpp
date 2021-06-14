a3bcb046b5bb2a9d454f20f1e7cf04386cb941c4
Gaurav Singh <0201csai117@niet.co.in>
2021-06-15 00:46:03 +0530

    commit

--- 8.3.1 Sorting in Arrays Selection Sort C++/Selection sort.cpp
+++ 8.3.1 Sorting in Arrays Selection Sort C++/Selection sort.cpp
@@ -14,5 +14,22 @@ int main()
         cin >> arr[i];
     }
 
-    return 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 1 + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
