4b446ef733bb1d5b0156c0e7a263edb029a738fb
Gaurav Singh <0201csai117@niet.co.in>
2021-06-14 17:10:28 +0530

    commit

--- 8.1 Introduction to Arrays in C++/Array.cpp
+++ 8.1 Introduction to Arrays in C++/Array.cpp
@@ -3,9 +3,20 @@ using namespace std;
 
 int main()
 {
-    int array[4] = {10, 20, 30, 40};
+    int n;
+    cin >> n;
 
-    cout << array[3] << endl;
+    int array[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << array[i] << " ";
+    }
 
         return 0;
 }
