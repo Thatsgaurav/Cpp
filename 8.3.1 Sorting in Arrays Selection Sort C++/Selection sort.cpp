10baad454fc90386edfcd802171ba49154b2dffe
Gaurav Singh <0201csai117@niet.co.in>
2021-06-15 00:30:27 +0530

    commit

--- 8.3.1 Sorting in Arrays Selection Sort C++/Selection sort.cpp
+++ 8.3.1 Sorting in Arrays Selection Sort C++/Selection sort.cpp
@@ -1,1 +1,18 @@
 // Selection Sort
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    return 0;
+}
