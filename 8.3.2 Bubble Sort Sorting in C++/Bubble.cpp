898d9e072c19aedf1c0cd3d7b82a5f59ebd29976
Gaurav Singh <0201csai117@niet.co.in>
2021-06-15 01:11:49 +0530

    commit

--- 8.3.2 Bubble Sort Sorting in C++/Bubble.cpp
+++ 8.3.2 Bubble Sort Sorting in C++/Bubble.cpp
@@ -14,5 +14,24 @@ int main()
         cin >> arr[i];
     }
 
-    return 0;
+    int counter = 1;
+    while (counter < n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        counter++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
