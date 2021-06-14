082fbb02a746797d73465c3d00fde9086858d60d
Gaurav Singh <0201csai117@niet.co.in>
2021-06-14 17:13:24 +0530

    commit

--- 8.1 Introduction to Arrays in C++/Problems/Max and Min.cpp
+++ 8.1 Introduction to Arrays in C++/Problems/Max and Min.cpp
@@ -5,8 +5,16 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
     
-    
-    return 0;
+
+        return 0;
 }
