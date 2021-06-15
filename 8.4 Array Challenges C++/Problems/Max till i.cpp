d91a460f76660c16f8f9991d3bd4693637c1e1c8
Gaurav Singh <0201csai117@niet.co.in>
2021-06-15 13:03:02 +0530

    commit

--- 8.4 Array Challenges C++/Problems/Max till i.cpp
+++ 8.4 Array Challenges C++/Problems/Max till i.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
+    int mx = -19999999;
+    int n;
+    cin >> n;
 
-    return 0;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n;)
+
+        return 0;
 }
