cb014a66f819777cb3bb708ef7576e8cc8b564ec
Gaurav Singh <0201csai117@niet.co.in>
2021-06-20 22:57:26 +0530

    commit

--- 8.5 Arrays - Q's asked by Top MNC's  C++/Problems/subarray.cpp
+++ 8.5 Arrays - Q's asked by Top MNC's  C++/Problems/subarray.cpp
@@ -1,1 +1,10 @@
 // Given an unsorted array A of size N of non - negative integers, find a continuous subarray which adds to a given number S.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
