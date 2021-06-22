0ca86520b727e5ffc8e9f1a315019b44411af4ea
Gaurav Singh <0201csai117@niet.co.in>
2021-06-22 23:05:39 +0530

    commit

--- 8.5 Arrays - Q's asked by Top MNC's  C++/Problems/smallest positive.cpp
+++ 8.5 Arrays - Q's asked by Top MNC's  C++/Problems/smallest positive.cpp
@@ -1,2 +1,11 @@
 // You are given an array arr[] of N integers including 0. The task is to find the smallest
 // positive number missing from the array.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
